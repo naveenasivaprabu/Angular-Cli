2452eb1603f3326192dc870754589ec768e822d1
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2017-04-21 08:50:48 +0530

    Added Component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  myName = "My Name Value Dynamic";
+  innerHtmlValue = "<h7>Hello World !!</h7>";
+  profile;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submit(){
+    alert('Submitted');
+    this.profile="Value chnage from class"
   }
 
 }
